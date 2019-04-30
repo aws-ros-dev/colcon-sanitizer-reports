ec523e941ff391dde730c44717c20ede841c3adb
Cameron Evans <cev@amazon.com>
2019-04-30 16:49:52 -0700

    Adds sanitizer_log_parser.py and tests
    Sanitizer log parser is fed lines from "colcon test" log output and
    finds all sanitizer warnings and errors. Basic information about the
    errors is reported in a csv. Information includes.
    - package: name of the package where error occurred
    - error_name: name of the sanitizer error
    - stack_trace_key: a single line from a relevant stack trace that in the error
    - count: aggregate of (package, error_name, stack_trace_key)

--- colcon_sanitizer_reports/sanitizer_log_parser.py
+++ colcon_sanitizer_reports/sanitizer_log_parser.py
@@ -1,165 +1,94 @@
+# Copyright 2019 Open Source Robotics Foundation
+# Licensed under the Apache License, version 2.0
+
 from collections import defaultdict
 import csv
 from io import StringIO
 import re
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, NamedTuple, Optional
+
+from colcon_sanitizer_reports._sanitizer_section import SanitizerSection
+
+# The start line of a section can be found with the following regex. Additionally, any prefix that
+# is prepended by the logging system can be extracted and be used to lstrip following section lines.
+_FIND_SECTION_START_LINE_REGEX = \
+    re.compile(r'^(?P<prefix>.*?)(==\d+==|)(WARNING|ERROR):.*Sanitizer:.*$')
+
+# The end line of a section can be found with the following regex. Additionally, any prefix that is
+# prepended by the logging system can be extracted and be used to match the common prefix of the
+# section to which the end line belongs.
+_FIND_SECTION_END_LINE_REGEX = re.compile(r'^(?P<prefix>.*?)(SUMMARY: .*Sanitizer: .*)$')
 
 
-class _KeysFactory:
-    """Finds key stack trace lines in sanitizer error subsections."""
+class SanitizerLogParserOutputPrimaryKey(NamedTuple):
+    """SanitizerLogParser report output is keyed on these fields.
 
-    @staticmethod
-    def _make_masked_line(line: str) -> str:
-        """Masks numbers in a sanitizer error line so that keys from similar errors will match."""
-        masked_line = line
-        masked_line = re.sub(r'0x[\dabcdef]+', '0xX', masked_line)
-        masked_line = re.sub(r' \d+ ', ' X ', masked_line)
-        masked_line = re.sub(r'#\d+', '#X', masked_line)
+    This is the primary key of the report. Each unique combination of these fields is reported on
+    its own line and no two output lines will have a duplicate combination of these fields.
 
-        return masked_line
+    After initialization, SanitizerLogParserOutputPrimaryKey includes the following data members.
 
-    @staticmethod
-    def _make_keys_base(*, lines: Tuple[str], stack_begin_regexes: Tuple[str, ...]) -> Tuple[str]:
-        keys: List[str] = []
+    package:
+        Name of the ros2 package where the error occurred.
 
-        lines = iter(lines)
-        for stack_begin_regex in stack_begin_regexes:
-            for line in lines:
-                match = re.match(
-                    stack_begin_regex,
-                    _KeysFactory._make_masked_line(line)
-                )
-                if match is not None:
-                    break
+    error_name:
+        Name of the sanitizer error (such as "data race", "lock-order-inversion", etc). See
+        SanitizerSection for more details.
 
-            for line in lines:
-                match = re.match(
-                    r'^\s*#X (0xX in|)\s*(?P<key>.*/ros2.*)\s*$',
-                    _KeysFactory._make_masked_line(line)
-                )
-                if match is not None:
-                    keys.append(match.groupdict()['key'])
-                    break
-            else:
-                # We didn't find one of the keys we were required to find.
-                keys = []
-                break
+    stack_trace_key:
+        The key of a significant stack trace. Note that a single sanitizer error/warning section may
+        have multiple significant stack traces, resulting in multiple keys and thus, multiple
+        SanitizerLogParserOutputPrimaryKeys. See SanitizerSectionPart and
+        SanitizerSectionPartStackTrace for more details.
+    """
 
-        return tuple(keys)
-
-    @staticmethod
-    def _make_default_keys(*, lines: Tuple[str]) -> Tuple[str]:
-        # Most sanitizer errors have the only/most significant stack trace first in a subsection, so
-        # we assume we've already found the correct stack trace.
-        return _KeysFactory._make_keys_base(
-            lines=lines,
-            stack_begin_regexes=(
-                r'^.*$',
-            ),
-        )
-
-    @staticmethod
-    def _make_detected_memory_leaks_keys(*, lines: Tuple[str]) -> Tuple[str]:
-        # There is one stack trace per subsection in a "detected memory leaks" error.
-        return _KeysFactory._make_keys_base(
-            lines=lines,
-            stack_begin_regexes=(
-                r'^Direct leak of X byte\(s\) in X object\(s\) allocated from:$',
-            )
-        )
-
-    @staticmethod
-    def _make_data_race_keys(*, lines: Tuple[str]) -> Tuple[str]:
-        # There are two stack traces involved a "data race" error, both in the same subsection.
-        return _KeysFactory._make_keys_base(
-            lines=lines,
-            stack_begin_regexes=(
-                r'^\s+(Read|Write) of size X at 0xX .*$',
-                r'^\s+Previous (read|write) of size X at 0xX .*$'
-            )
-        )
-
-    @staticmethod
-    def _make_lock_order_inversion_keys(*, lines: Tuple[str]) -> Tuple[str]:
-        # There are two stack traces involved in a "lock-order-inversion" error, both in the same
-        # subsection and with identical header lines.
-        return _KeysFactory._make_keys_base(
-            lines=lines,
-            stack_begin_regexes=(
-                r'^\s+Mutex M\d+ acquired here while holding mutex M\d+ in .*$',
-                r'^\s+Mutex M\d+ acquired here while holding mutex M\d+ in .*$'
-            )
-        )
-
-    @staticmethod
-    def make_keys(*, lines: Tuple[str], error_name: str) -> Tuple[str]:
-        if error_name == 'data race':
-            return _KeysFactory._make_data_race_keys(lines=lines)
-        elif error_name == 'detected memory leaks':
-            return _KeysFactory._make_detected_memory_leaks_keys(lines=lines)
-        elif error_name == 'lock-order-inversion':
-            return _KeysFactory._make_lock_order_inversion_keys(lines=lines)
-        else:
-            return _KeysFactory._make_default_keys(lines=lines)
-
-
-class _SubSection:
-    keys: Tuple[str]
-
-    def __init__(self, *, lines: Tuple[str], error_name: str) -> None:
-        self.keys = _KeysFactory.make_keys(lines=lines, error_name=error_name)
-
-
-class _Section:
-    """Contents for a single Sanitizer output section."""
-
+    package: str
     error_name: str
-    sub_sections: Tuple[_SubSection]
-
-    def __init__(self, *, lines: Tuple[str]) -> None:
-        # Section name comes after 'Sanitizer: ', and before any open paren or hex number.
-        self.error_name = re.match(
-            r'^.*Sanitizer: (?P<error_name>.+?)( \(| 0x[\dabcdef]+|\s*$)', lines[0]
-        ).groupdict()['error_name']
-
-        # Divide into _SubSections. SubSections begin with a line that is not indented.
-        sub_section_lines: List[str] = []
-        sub_sections: List[_SubSection] = []
-        for line in lines:
-            if re.match(r'^\S.*$', line) is not None and sub_section_lines:
-                sub_sections.append(
-                    _SubSection(lines=tuple(sub_section_lines), error_name=self.error_name)
-                )
-                sub_section_lines = []
-
-            sub_section_lines.append(line)
-        else:
-            sub_sections.append(
-                _SubSection(lines=tuple(sub_section_lines), error_name=self.error_name)
-            )
-
-        self.sub_sections = tuple(sub_sections)
+    stack_trace_key: str
 
 
 class SanitizerLogParser:
-    """Parses logged sanitizer errors and warnings and generates report."""
+    """Parses sanitizer error and warning sections from a log and generates a summary report.
+
+    The parser works with the output of "colcon test" and reports information about all sanitizer
+    (eg. AddressSanitizer or ThreadSanitizer) error and warning output. Every sanitizer error and
+    warning occurs during tests for some ros2 package, has a specific error name, and has one or
+    more significant stack traces that are helpful for determining the root cause of the error or
+    warning.
+
+    Lines from "colcon test" output should be added to the parser one at a time with the add_line()
+    method. When finished, the report can be access from the csv property.
+
+    CSV output columns are "package,error_name,stack_trace_key,count". Package, error_name, and
+    stack_trace_key columns make up the primary key for CSV output. See SanitizerLogParserOutputPrimaryKey for more
+    details about the primary key.
+
+    CSV output additional columns include:
+
+    count:
+        The count of times the fields from the primary key occur while parsing the log.
+    """
 
     def __init__(self) -> None:
-        # Holds count of errors seen. This is what will be in the report.
-        self._counts = defaultdict(int)
+        # Holds count of errors seen for each output key (.
+        self._count_by_output_primary_key = defaultdict(int)
 
-        # Current package and sections that are partially parsed
+        # Current package output that is being parsed
         self._package: Optional[str] = None
-        self._section_lines_by_prefix: Dict[str: List[str]] = {}
+
+        # We keep lines for partially-gathered sanitizer sections here. Incoming lines that match
+        # one of the find_line_regexes is appended to the associated list of lines.
+        # noinspection PyUnresolvedReferences
+        self._lines_by_find_line_regex: Dict[re.Pattern, List[str]] = {}
 
     @property
     def csv(self) -> str:
         """Return a csv representation of reported error/warnings."""
         csv_f_out = StringIO()
         writer = csv.writer(csv_f_out)
-        writer.writerow(['package', 'error_name', 'key', 'count'])
-        for (package, error_name, key), count in self._counts.items():
-            writer.writerow([package, error_name, key, count])
+        writer.writerow([*SanitizerLogParserOutputPrimaryKey._fields, 'count'])
+        for output_key, count in self._count_by_output_primary_key.items():
+            writer.writerow([*output_key, count])
 
         return csv_f_out.getvalue()
 
@@ -167,29 +96,36 @@ class SanitizerLogParser:
         self._package = package
 
     def add_line(self, line: str) -> None:
-        """Generate report from log file lines."""
         line = line.rstrip()
 
-        # If we have a new sanitizer section, start gathering lines for it.
-        match = re.match(r'^(?P<prefix>.*?)(==\d+==|)(WARNING|ERROR):.*Sanitizer:.*', line)
+        # If we have a sanitizer section starting line, start gathering lines for it.
+        match = re.match(_FIND_SECTION_START_LINE_REGEX, line)
         if match is not None:
+            # Future lines for this new sanitizer section are sometimes interleaved with unrelated
+            # log lines due to multi-threaded logging. The log lines we care about will have the
+            # same prefix, so they will match the following pattern with the prefix included.
             prefix = match.groupdict()['prefix']
-            self._section_lines_by_prefix[prefix] = []
+            find_line_regex = re.compile(r'^{prefix}(?P<line>.*)$'.format(prefix=re.escape(prefix)))
+            self._lines_by_find_line_regex[find_line_regex] = []
 
-        # Check if this line belongs to any of the sections we're currently building.
-        for prefix in self._section_lines_by_prefix.keys():
-            match = re.match(r'^{prefix}(?P<line>.*)$'.format(prefix=re.escape(prefix)), line)
+        # If this line belongs to one of the sections we're currently building, append it to lines
+        # for that section.
+        for find_line_regex, lines in self._lines_by_find_line_regex.items():
+            match = re.match(find_line_regex, line)
             if match is not None:
-                self._section_lines_by_prefix[prefix].append(match.groupdict()['line'])
-                break
+                lines.append(match.groupdict()['line'])
 
-        # If this is the last line of a section, create the section and stop gathering lines for it.
-        match = re.match(r'^(?P<prefix>.*)(SUMMARY: .*Sanitizer: .*)$', line)
-        if match is not None:
-            prefix = match.groupdict()['prefix']
-            section = _Section(lines=tuple(self._section_lines_by_prefix[prefix]))
-            for sub_section in section.sub_sections:
-                for key in sub_section.keys:
-                    self._counts[(self._package, section.error_name, key)] += 1
-            del self._section_lines_by_prefix[prefix]
-            return
+                # If this is the last line of a section, create the section and stop gathering lines
+                # for it.
+                match = re.match(_FIND_SECTION_END_LINE_REGEX, line)
+                if match is not None:
+                    section = SanitizerSection(lines=tuple(lines))
+                    for part in section.parts:
+                        for relevant_stack_trace in part.relevant_stack_traces:
+                            output_primary_key = SanitizerLogParserOutputPrimaryKey(
+                                self._package, section.error_name, relevant_stack_trace.key
+                            )
+                            self._count_by_output_primary_key[output_primary_key] += 1
+                    del self._lines_by_find_line_regex[find_line_regex]
+
+                break
