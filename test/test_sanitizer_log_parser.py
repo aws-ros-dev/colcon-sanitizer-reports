3f9e919e974b96b24b4d43288a9c1f40fa75287f
Cameron Evans <cev@amazon.com>
2019-04-30 16:49:52 -0700

    Adds sanitizer_log_parser.py and tests
    Sanitizer log parser is fed lines from "colcon test" log output and
    finds all sanitizer warnings and errors. Basic information about the
    errors is reported in a csv. Information includes.
    - package: name of the package where error occurred
    - error_name: name of the sanitizer error
    - stack_trace_key: a single line from a relevant stack trace that in the error
    - count: aggregate of (package, error_name, stack_trace_key)

--- test/test_sanitizer_log_parser.py
+++ test/test_sanitizer_log_parser.py
@@ -1,3 +1,6 @@
+# Copyright 2019 Open Source Robotics Foundation
+# Licensed under the Apache License, version 2.0
+
 from csv import DictReader
 import os
 from typing import Dict, Optional, Tuple
@@ -5,42 +8,54 @@ from typing import Dict, Optional, Tuple
 from colcon_sanitizer_reports.sanitizer_log_parser import SanitizerLogParser
 import pytest
 
+# Directory names of resources in test/resources. Directories should include 'input.log' and
+# 'expected_output.csv'.
+_RESOURCE_NAMES = (
+    'data_race_and_lock_order_inversion_interleaved_output',
+    'data_race_different_keys',
+    'detected_memory_leaks_multiple_subsections_direct_and_indirect_leaks',
+    'lock_order_inversion_same_key',
+    'no_errors',
+    'segv',
+)
 
-class Fixture:
 
+class SanitizerLogParserFixture:
     def __init__(self, resource_name: str) -> None:
         self.resource_name = resource_name
-        self._parser: Optional[SanitizerLogParser] = None
+        self._sanitizer_log_parser: Optional[SanitizerLogParser] = None
 
     @property
     def resource_path(self) -> str:
-        return os.path.dirname(os.path.abspath(__file__)) + '/resources/' + self.resource_name
+        return os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'resources', self.resource_name
+        )
 
     @property
     def input_log_path(self) -> str:
-        return self.resource_path + '/input.log'
+        return os.path.join(self.resource_path, 'input.log')
 
     @property
     def expected_output_csv_path(self) -> str:
-        return self.resource_path + '/expected_output.csv'
+        return os.path.join(self.resource_path, 'expected_output.csv')
 
     @property
-    def parser(self) -> SanitizerLogParser:
-        if self._parser is None:
+    def sanitizer_log_parser(self) -> SanitizerLogParser:
+        if self._sanitizer_log_parser is None:
             parser = SanitizerLogParser()
             parser.set_package(self.resource_name)
             with open(self.input_log_path, 'r') as input_log_f_in:
                 for line in input_log_f_in.readlines():
                     parser.add_line(line)
 
-            self._parser = parser
+            self._sanitizer_log_parser = parser
             parser.set_package(None)
 
-        return self._parser
+        return self._sanitizer_log_parser
 
     @property
     def report_csv(self) -> DictReader:
-        return DictReader(self.parser.csv.split('\n'))
+        return DictReader(self.sanitizer_log_parser.csv.split('\n'))
 
     @property
     def expected_csv(self) -> DictReader:
@@ -48,31 +63,30 @@ class Fixture:
             return DictReader(expected_output_csv_f_in.read().split('\n'))
 
 
-@pytest.fixture(params=os.listdir(os.path.dirname(os.path.abspath(__file__)) + '/resources'))
-def fixture(request) -> Fixture:
-    return Fixture(request.param)
+@pytest.fixture(params=_RESOURCE_NAMES)
+def sanitizer_log_parser_fixture(request) -> SanitizerLogParserFixture:
+    return SanitizerLogParserFixture(request.param)
 
 
-def test_csv_has_field_names(fixture) -> None:
-    assert tuple(fixture.report_csv.fieldnames) == ('package', 'error_name', 'key', 'count')
-
-
-def test_csv_has_output(fixture) -> None:
-    if fixture.resource_name == 'no_errors':
-        assert len(list(fixture.report_csv)) == 0
+def test_csv_has_output(sanitizer_log_parser_fixture) -> None:
+    if sanitizer_log_parser_fixture.resource_name == 'no_errors':
+        assert len(list(sanitizer_log_parser_fixture.report_csv)) == 0
     else:
-        assert len(list(fixture.report_csv)) > 0
+        assert len(list(sanitizer_log_parser_fixture.report_csv)) > 0
 
 
-def test_csv_has_expected_line_count(fixture) -> None:
-    assert len(list(fixture.report_csv)) == len(list(fixture.expected_csv))
+def test_csv_has_expected_line_count(sanitizer_log_parser_fixture) -> None:
+    assert len(list(sanitizer_log_parser_fixture.report_csv)) == \
+           len(list(sanitizer_log_parser_fixture.expected_csv))
 
 
-def test_csv_has_expected_lines(fixture) -> None:
+def test_csv_has_expected_lines(sanitizer_log_parser_fixture) -> None:
     def make_key(line: Dict[str, str]) -> Tuple[str, ...]:
         return tuple(line.items())
 
-    expected_line_by_key = {make_key(line): line for line in fixture.expected_csv}
+    expected_line_by_key = {
+        make_key(line): line for line in sanitizer_log_parser_fixture.expected_csv
+    }
 
-    for line in fixture.report_csv:
+    for line in sanitizer_log_parser_fixture.report_csv:
         assert line == expected_line_by_key.pop(make_key(line))
